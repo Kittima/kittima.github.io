edbd3561c3b866cda74524a60f7026bad38ac90d
Kittima Mekhabunchakij <kittima.me@gmail.com>
2016-04-12 04:45:34 +0700

    first recommit - github.io

---
+++ code/simple.py
@@ -0,0 +1,16 @@
+""" here is a dummy documentation """
+import os
+
+def square(x):
+    '''
+	short description of the function square longish explanation: returns the square of x: :math:`x^2`
+
+    :param a: an input argument
+
+    :returns: x*x
+	
+    '''
+	
+    return x*x
+
+assert 4 == square(2)
